d2c668fb7b0c964ef0d3f27c21617e08aa2fd547
hjh010806 <010806hjh@naver.com>
2024-03-15 15:42:06 +0900

    testDay1

---
+++ src/ExamTest/Exam2.java
@@ -0,0 +1,23 @@
+package ExamTest;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        star(3);
+        star(5);
+        star(7);
+
+
+    }
+    static void star(int count){
+        int count2 = count + 2;
+        for (int i = 0; i < count2; i += 2) {
+            for (int j = count2; j > i; j -=2 ){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
